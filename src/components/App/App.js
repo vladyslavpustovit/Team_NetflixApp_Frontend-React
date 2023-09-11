d02dae1baf37955a6832a42861c9da92c9d44d09
vladyslavpustovit <Vladyslav.Pustovit@student.intecbrussel.be>
2023-09-11 09:30:31 +0200

    Login and Sign Up pages.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,7 @@
-import Footer from "../footer/footer";
-import Header from "../header/header";
 import AppRouter from "../../routing-module";
-
 export default function App() {
-    const pathname = window.location.pathname;
 
-    // Routes without header and footer
-    const excludeRoutes = ["/landing", "/login", "/register"];
-
-    // Boolean value to show or hide header and footer
-    const showHeaderAndFooter = !excludeRoutes.includes(pathname);
     return (
-        <>
-            {showHeaderAndFooter && <Header />}
-                <AppRouter/>
-            {showHeaderAndFooter && <Footer />}
-        </>
+        <AppRouter/>
     )
 }
--- src/components/App/register/register.js
+++ src/components/App/register/register.js
@@ -1,9 +1,104 @@
-function Register() {
+import {useState} from "react";
+import {useSignup} from "../../../hooks/useSignup";
+import {Link} from "react-router-dom";
+export default function Register() {
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const { signup, error, isLoading } = useSignup();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await signup(email, username, password);
+        // window.location.pathname = "/"
+    }
+
     return (
-        <>
-            <h1>Register</h1>
-        </>
+        <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8 text-white">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img
+                    className="mx-auto h-32 w-auto"
+                    src="/assets/images/logo.png"
+                    alt="Netflix"
+                />
+                <h2 className="text-center text-2xl font-bold leading-9 tracking-tight">
+                    Create a new account
+                </h2>
+            </div>
+
+            <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form className="space-y-6" onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium leading-6">
+                            Email
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                autoComplete="email"
+                                required
+                                onChange={(e)=>setEmail(e.target.value)}
+                                className="block w-full rounded-md border-0 py-1.5 pl-2 bg-gray-700 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-white"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="username" className="block text-sm font-medium leading-6">
+                            Username
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="username"
+                                name="username"
+                                type="username"
+                                autoComplete="username"
+                                required
+                                onChange={(e)=>setUsername(e.target.value)}
+                                className="block w-full rounded-md border-0 py-1.5 pl-2 bg-gray-700 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-white"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="password" className="block text-sm font-medium leading-6">
+                                Password
+                            </label>
+                        </div>
+                        <div className="mt-2">
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                autoComplete="current-password"
+                                required
+                                onChange={(e)=>setPassword(e.target.value)}
+                                className="block w-full rounded-md border-0 py-1.5 pl-2 bg-gray-700 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-white"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            disabled={isLoading}
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Sign up
+                        </button>
+                        {error && <div className="text-red-700 mt-2 text-sm">{error}</div>}
+                    </div>
+                </form>
+
+                <p className="mt-10 text-center text-sm text-gray-500">
+                    Already have an account?{' '}
+                    <Link to="/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                       Sign In
+                    </Link>
+                </p>
+            </div>
+        </div>
     );
 }
-
-export default Register;
