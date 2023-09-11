23fd5bcdd1d222fa5eeb9fb0a255d1969422317f
vladyslavpustovit <Vladyslav.Pustovit@student.intecbrussel.be>
2023-09-11 10:10:40 +0200

    Merge with master

---
+++ src/components/App/land-page/navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+const Navbar = ()=>{
+  return(
+    <>
+    <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
+        <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
+        <div>
+            <Link to='/login'>
+                <button className='text-white pr-4 cursor-pointer '>Sign In</button>
+            </Link>
+            <Link to='/register'>
+                <button className='bg-red-600 px-6 rounded cursor-pointer text-white'>Sign Up</button>
+            </Link>
+        </div>
+    </div>
+    </>
+
+  )
+    
+
+}
+
+export default Navbar
