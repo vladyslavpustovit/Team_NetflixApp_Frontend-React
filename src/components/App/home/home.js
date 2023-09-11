8e747ee1775a730d89f73c77a0e0f9ab98cd025b
Khalid <114026496+loodi181@users.noreply.github.com>
2023-09-11 15:41:45 +0200

    added dynamic cards, css , footer

--- src/components/App/home/home.js
+++ src/components/App/home/home.js
@@ -1,12 +1,67 @@
 import './home.css';
-import Cards from '../../cards/Cards'
+import React, { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
 
-function Home() {
+import { useAuthContext } from '../../../hooks/useAuthContext';
+import { MovieCard } from '../../App/content/movieCard';
+
+export default function Home() {
+  const { user } = useAuthContext();
+  const [movies, setMovies] = useState([]);
+
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 16;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/movies", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${user.token}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const json = await response.json();
+        setMovies(json);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+// pagination 
+  const totalPages = Math.ceil(movies.length / itemsPerPage);
+  const startIndex = currentPage * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const subset = movies.slice(startIndex, endIndex);
+  const handlePageChange = (selectedPage) => {
+    setCurrentPage(selectedPage.selected);
+  };
+
   return (
-      <>
-          <Cards />
-      </>
+    <div className=''>
+    {/* cards part  */}
+      <div className="grid grid-cols-4 gap-5 px-6">
+        {subset.map((data) => (
+          <MovieCard key={data.id} movie={data} />
+          ))}
+      </div>
+
+          {/* pagination part */}
+      <div className='parent px-6'>
+        <ReactPaginate
+          className="pagi "
+          pageCount={totalPages}
+          onPageChange={handlePageChange}
+          forcePage={currentPage}
+        />
+      </div>
+    </div>
   );
 }
-
-export default Home;
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,9 +1,48 @@
+import './footer.css'
+
 function Footer() {
-    return (
-        <>
-            <h1>Footer</h1>
-        </>
-    );
+ 
+  return (
+    <>
+      <footer>
+        <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+          <div class="sm:flex sm:items-center sm:justify-between">
+            <img src="/assets/images/logo.png" class="h-12 mr-3" alt="Logo" />
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+              <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6 ">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6">
+                  Privacy Policy
+                </a>
+              </li>
+              <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6 ">
+                  Licensing
+                </a>
+              </li>
+              <li>
+                <a href="#" class="hover:underline">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+          <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2023{" "}
+            <a href="#" class="hover:underline">
+              Repo™
+            </a>
+            . All Rights Reserved.
+          </span>
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default Footer;
